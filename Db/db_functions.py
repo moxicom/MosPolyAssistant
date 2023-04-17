553f96ec80400966c3c90ce78c308db3d1f2f2f7
Sof <borsoffei@gmail.com>
2023-04-17 17:32:19 +0300

    Теги. Добавление их к сообщению. Небольшой декоративное изменение файла с функциями (рефакторинг)

--- Db/db_functions.py
+++ Db/db_functions.py
@@ -3,36 +3,31 @@ from sqlalchemy.sql import select
 from sqlalchemy import Column, String, Integer, Table, MetaData, VARCHAR, Date
 from sqlalchemy.orm import declarative_base as base
 import asyncio
+from sqlalchemy import update
 
 # myBase = base()
 engine = asyncio_ext.create_async_engine(
     "postgresql+asyncpg://postgres:314159@localhost:5432/postgres",
     echo=False,
     future=True
-    )
+)
 
 
 async def createTable():
     metadata = MetaData()
     table = Table("messages", metadata,
-                        Column('id', Integer, primary_key=True),
-                        Column('group_id', Integer),
-                        Column('title', VARCHAR(255)),
-                        Column('text', String),
-                        Column('tag_id', Integer),
-                        Column('images', String),
-                        Column('videos', String),
-                        Column('files', String),
-                        Column('created_at', Date))
+                  Column('id', Integer, primary_key=True),
+                  Column('group_id', Integer),
+                  Column('title', VARCHAR(255)),
+                  Column('text', String),
+                  Column('tag_id', Integer),
+                  Column('images', String),
+                  Column('videos', String),
+                  Column('files', String),
+                  Column('created_at', Date))
     async with engine.begin() as conn:
         await conn.run_sync(metadata.create_all)
         await conn.commit()
-    
-
-
-
-
-
 
 
 ########################### users #######################
@@ -40,10 +35,10 @@ async def insert_users(name: VARCHAR, group_id: int, tg_id: int):
     async with engine.begin() as conn:
         metadata = MetaData()
         table = Table('users', metadata,
-                        Column('id', Integer, primary_key=True),
-                        Column('name', VARCHAR),
-                        Column('group_id', Integer),
-                        Column('tg_id', Integer))
+                      Column('id', Integer, primary_key=True),
+                      Column('name', VARCHAR),
+                      Column('group_id', Integer),
+                      Column('tg_id', Integer))
         insertStmt = table.insert().values(name=name, group_id=group_id, tg_id=tg_id)
         await conn.execute(insertStmt)
         await conn.commit()
@@ -53,10 +48,10 @@ async def fetch_users(tg_id: int):
     async with engine.connect() as conn:
         metadata = MetaData()
         table = Table('users', metadata,
-                        Column('id', Integer, primary_key=True),
-                        Column('name', VARCHAR),
-                        Column('group_id', Integer),
-                        Column('tg_id', Integer))
+                      Column('id', Integer, primary_key=True),
+                      Column('name', VARCHAR),
+                      Column('group_id', Integer),
+                      Column('tg_id', Integer))
         selectStmt = select(table).where(table.c.tg_id == tg_id)
         result = await conn.execute(selectStmt)
         users = result.fetchall()
@@ -68,19 +63,13 @@ async def delete_users(tg_id: int):
     async with engine.connect() as conn:
         metadata = MetaData()
         table = Table('users', metadata,
-                        Column('id', Integer, primary_key=True),
-                        Column('name', VARCHAR),
-                        Column('group_id', Integer),
-                        Column('tg_id', Integer))
+                      Column('id', Integer, primary_key=True),
+                      Column('name', VARCHAR),
+                      Column('group_id', Integer),
+                      Column('tg_id', Integer))
         deleteStmt = table.delete().where(table.c.tg_id == tg_id)
         await conn.execute(deleteStmt)
         await conn.commit()
-    
-
-
-
-
-
 
 
 ########################### groups_info #################
@@ -88,28 +77,29 @@ async def insert_groups_info(group_name: VARCHAR, password: VARCHAR):
     async with engine.begin() as conn:
         metadata = MetaData()
         table = Table("groups_info", metadata,
-                        Column('id', Integer, primary_key=True),
-                        Column('name', VARCHAR(50)),
-                        Column('password', VARCHAR))
+                      Column('id', Integer, primary_key=True),
+                      Column('name', VARCHAR(50)),
+                      Column('password', VARCHAR))
         insertStmt = table.insert().values(name=group_name, password=password)
         await conn.execute(insertStmt)
         await conn.commit()
         print('--------------' + 'groups info commited' + '------------')
-    
+
 
 async def fetch_groups_info(group_name: VARCHAR):
     async with engine.connect() as conn:
         metadata = MetaData()
         table = Table("groups_info", metadata,
-                        Column('id', Integer, primary_key=True),
-                        Column('name', VARCHAR(50)),
-                        Column('password', VARCHAR))
+                      Column('id', Integer, primary_key=True),
+                      Column('name', VARCHAR(50)),
+                      Column('password', VARCHAR))
         selectStmt = select(table).where(table.c.name == group_name)
         result = await conn.execute(selectStmt)
         group_info = result.fetchall()
         print('--------------------------\n fetch_groups_info result = \n', group_info, '\n--------------------------')
         return group_info
- 
+
+
 async def change_group_info(id: int, field: str, new_value: str):
     """Изменение параметра в базе данных по id"""
     async with engine.connect() as conn:
@@ -118,8 +108,8 @@ async def change_group_info(id: int, field: str, new_value: str):
                       Column('id', Integer, primary_key=True),
                       Column('name', VARCHAR(20)),
                       Column('password', VARCHAR(20))
-                     )
-      
+                      )
+
         # Проверка на наличие значения в параметре id
         stmt = select(table).where(table.c.id == id)
         result = await conn.execute(stmt)
@@ -129,19 +119,14 @@ async def change_group_info(id: int, field: str, new_value: str):
 
         # Внесение изменений
         try:
-            update_stmt = table.update().where(table.c.id == id).values({field:new_value})
+            update_stmt = table.update().where(table.c.id == id).values({field: new_value})
             await conn.execute(update_stmt)
             await conn.commit()
-            return 0
             print('--------------' + 'group info changed' + '------------')
+            return 0
+
         except:
             return -1
-    
-
-
-
-
-
 
 
 ###########################  groups_members ####################
@@ -149,10 +134,10 @@ async def insert_groups_members(member_id: int, group_id: int, role: int):
     async with engine.begin() as conn:
         metadata = MetaData()
         table = Table("groups_members", metadata,
-                        Column('id', Integer, primary_key=True),
-                        Column('member_id', Integer),
-                        Column('group_id', Integer),
-                        Column('role', Integer))
+                      Column('id', Integer, primary_key=True),
+                      Column('member_id', Integer),
+                      Column('group_id', Integer),
+                      Column('role', Integer))
         insertStmt = table.insert().values(member_id=member_id, group_id=group_id, role=role)
         await conn.execute(insertStmt)
         await conn.commit()
@@ -162,30 +147,24 @@ async def fetch_groups_members(group_id: int = -1, member_id: int = -1):
     async with engine.connect() as conn:
         metadata = MetaData()
         table = Table("groups_members", metadata,
-                        Column('id', Integer, primary_key=True),
-                        Column('member_id', Integer),
-                        Column('group_id', Integer),
-                        Column('role', Integer))
-        
+                      Column('id', Integer, primary_key=True),
+                      Column('member_id', Integer),
+                      Column('group_id', Integer),
+                      Column('role', Integer))
+
         if (group_id == -1 and member_id != -1):
             selectStmt = select(table).where(table.c.member_id == member_id)
         elif (group_id != -1 and member_id != -1):
             selectStmt = select(table).where(table.c.group_id == group_id, table.c.member_id == member_id)
         elif (group_id != -1 and member_id == -1):
-            selectStmt = select(table).where(table.c.group_id == group_id)   
+            selectStmt = select(table).where(table.c.group_id == group_id)
         else:
             return Exception("NO ARGS")
-        
+
         result = await conn.execute(selectStmt)
         group_members = result.fetchall()
         print('-' * 15 + '\n\t fetch_groups_members result = \n\t', group_members, '\n' + '-' * 15)
         return group_members
-    
-
-
-
-
-
 
 
 ########################### tags ########################
@@ -193,41 +172,34 @@ async def insert_tags(group_id: int, name: str, parent_id: int):
     async with engine.begin() as conn:
         metadata = MetaData()
         table = Table("tags", metadata,
-                        Column('id', Integer, primary_key=True),
-                        Column('group_id', Integer),
-                        Column('name', VARCHAR),
-                        Column('parent_id', Integer))
+                      Column('id', Integer, primary_key=True),
+                      Column('group_id', Integer),
+                      Column('name', VARCHAR),
+                      Column('parent_id', Integer))
         insertStmt = table.insert().values(group_id=group_id, name=name, parent_id=parent_id)
         await conn.execute(insertStmt)
         await conn.commit()
-    
-
-
-
-
-
 
 
 ########################### messages ####################
-async def insert_messages(group_id: int, title: str, text: str, tag_id: int, images: str, videos: str, files: str, created_at):
+async def insert_messages(group_id: int, title: str, text: str, tag_id: int, images: str, videos: str, files: str,
+                          created_at):
     async with engine.begin() as conn:
         metadata = MetaData()
         table = Table("messages", metadata,
-                        Column('id', Integer, primary_key=True),
-                        Column('group_id', Integer),
-                        Column('title', VARCHAR),
-                        Column('text', VARCHAR),
-                        Column('tag_id', Integer),
-                        Column('images', VARCHAR),
-                        Column('videos', VARCHAR),
-                        Column('files', VARCHAR),
-                        Column('created_at', Date))
-        insertStmt = table.insert().values(group_id=group_id, title=title, text=text, tag_id=tag_id, images=images,\
-                                            videos=videos, files=files, created_at=created_at)
+                      Column('id', Integer, primary_key=True),
+                      Column('group_id', Integer),
+                      Column('title', VARCHAR),
+                      Column('text', VARCHAR),
+                      Column('tag_id', Integer),
+                      Column('images', VARCHAR),
+                      Column('videos', VARCHAR),
+                      Column('files', VARCHAR),
+                      Column('created_at', Date))
+        insertStmt = table.insert().values(group_id=group_id, title=title, text=text, tag_id=tag_id, images=images, \
+                                           videos=videos, files=files, created_at=created_at)
         await conn.execute(insertStmt)
         await conn.commit()
-
-
 
 # async def fetch_users(group_id: int):
 #     async with engine.connect() as conn:
---
+++ keyboards/admin/tags.py
@@ -0,0 +1,104 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from aiogram.utils import executor
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.types import ParseMode
+from aiogram.utils.markdown import text, quote_html
+
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.dispatcher.filters import Command
+
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+API_TOKEN = ''
+#
+# logging.basicConfig(level=logging.INFO)
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+existing_tags = []
+
+
+class States(StatesGroup):
+    MESSAGE = State()
+    CHOOSE_ACTION = State()
+    EXISTING_TAG = State()
+    NEW_TAG = State()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message, state: FSMContext):
+    markup = ReplyKeyboardRemove()
+    await state.finish()
+    await bot.send_message(chat_id=message.chat.id, text="Введите ваше сообщение:", reply_markup=markup)
+    await States.MESSAGE.set()
+
+
+@dp.message_handler(state=States.MESSAGE)
+async def ask_for_tag(message: types.Message, state: FSMContext):
+    print('Обработка сообщения')
+    user_text = message.text
+    markup = InlineKeyboardMarkup(row_width=2)
+    markup.add(InlineKeyboardButton("Существующий тег", callback_data="existing_tag"))
+    markup.add(InlineKeyboardButton("Добавить новый тег", callback_data="new_tag"))
+
+    await message.reply("Добавить тег?", reply_markup=markup)
+    await state.update_data(user_text=user_text)
+    await States.CHOOSE_ACTION.set()
+
+
+@dp.callback_query_handler(lambda c: c.data == "existing_tag", state=States.CHOOSE_ACTION)
+async def process_callback_existing_tag(callback_query: types.CallbackQuery, state: FSMContext):
+    print('кнопка существующий тег')
+    if existing_tags:
+        markup = InlineKeyboardMarkup(row_width=2)
+        for tag in existing_tags:
+            markup.add(InlineKeyboardButton(tag, callback_data=f"tag_{tag}"))
+        await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id,
+                                    text="Выберите тег:", reply_markup=markup)
+        await States.EXISTING_TAG.set()
+    else:
+        await bot.answer_callback_query(callback_query.id, "Нет существующих тегов, пожалуйста, добавьте новый тег.",
+                                        show_alert=True)
+
+
+@dp.callback_query_handler(lambda c: c.data == "new_tag", state=States.CHOOSE_ACTION)
+async def process_callback_new_tag(callback_query: types.CallbackQuery, state: FSMContext):
+    print('кнопка новый тег')
+    await bot.send_message(chat_id=callback_query.from_user.id, text="Введите новый тег:")
+    await States.NEW_TAG.set()
+
+
+@dp.message_handler(state=States.NEW_TAG)
+async def add_new_tag(message: types.Message, state: FSMContext):
+    print('добавляем новый тег')
+    new_tag = message.text
+    existing_tags.append(new_tag)
+    user_text = (await state.get_data()).get('user_text')
+    await message.reply(f"{user_text} {new_tag}")
+    await state.finish()
+
+
+@dp.callback_query_handler(state=States.EXISTING_TAG)
+async def process_callback_existing_tag(callback_query: types.CallbackQuery, state: FSMContext):
+    tag = callback_query.data[4:]
+    async with state.proxy() as data:
+        user_text = data['user_text']
+    tagged_message = f"{user_text} {tag}"
+    await bot.send_message(chat_id=callback_query.message.chat.id, text=tagged_message)
+    # await callback_query.answer()
+    await state.finish()
+
+
+# def main():
+#     from aiogram import executor
+#     executor.start_polling(dp, skip_updates=True)
+#
+#
+# if __name__ == '__main__':
+#     main()
