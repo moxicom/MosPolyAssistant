c098608445c44cd71e5a92fef14c66e48b3ed58a
Sof <borsoffei@gmail.com>
2023-05-17 15:36:53 +0300

    Вывод названия группы в списке участников

--- keyboards/admin/list_of_group.py
+++ keyboards/admin/list_of_group.py
@@ -32,9 +32,11 @@ async def send_list_of_group(callback_query: types.CallbackQuery, state: FSMCont
         group_id = await general.get_group_id_by_tg_id(tg_id=callback_query.from_user.id)
         users = await db.fetch_users_in_group(group_id=group_id)
         user_names = sorted([user[1] for user in users])
+        group_info = await db.fetch_group_info_by_id(group_id=group_id)
+        group_name = group_info[1]
         user_names_string = '\n'.join(f"{i + 1}. {name}" for i, name in enumerate(user_names))
         chat_id = callback_query.message.chat.id
-        await bot.send_message(chat_id=chat_id, text="Список группы:\n" + user_names_string)
+        await bot.send_message(chat_id=chat_id, text="Название группы:\n"+group_name+"\nСписок группы:\n" + user_names_string)
     except Exception as ex:
         logging.exception(ex)
         await bot.answer_callback_query(callback_query.id, text="Ошибка" + str(ex))
