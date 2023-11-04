329b5b561c99f23826a2d77a6501df4d256c95e1
Alexander Markov <45567992+moxicom@users.noreply.github.com>
2023-11-05 01:48:45 +0300

    Update tags_attachments.py

--- handlers/admin/tags_attachments.py
+++ handlers/admin/tags_attachments.py
@@ -30,7 +30,7 @@ async def ask_for_attachments_message(callback_query: types.CallbackQuery, state
 
 async def received_attachments_handler(message: types.Message, state: FSMContext):
     '''Receive attachments from user -> to learn more check function inside'''
-    await attachments.receive_message_with_media(message, state, reply_keyboard_text="Получить медиа")
+    await attachments.receive_message_with_media(message, state, reply_keyboard_text="Продолжить")
     
 
 async def end_process_click_handler (message: types.Message, state: FSMContext):
