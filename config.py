b1cd24bc3e10d3155e43f395862db09ce0ab2551
Alexander Markov <45567992+moxicom@users.noreply.github.com>
2023-11-04 18:21:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 #token = '5983840222:AAH5nLq35iCpMvSRBsD6v8p5TqhL_kmLSXU'
-token = '6855569017:AAGdX6uuZ0JOIeLeL0-YxMnqmmiwstkmBzo'
+token = '<token>'
 
 storage = MemoryStorage()
 bot = Bot(token=token)
