efa9bada84eedc521f91455618a7ec5df01f8574
Alexander Bykov <113583500+AlexanderGhosty@users.noreply.github.com>
2023-12-01 20:14:24 +0300

    Comment added

--- handlers/common/tag_system_functions.py
+++ handlers/common/tag_system_functions.py
@@ -43,10 +43,14 @@ async def delete_tag(callback_query: types.CallbackQuery, state: FSMContext):
     else:
         tag_id = None
 
+    # Selecting a tag to delete  
     if parameters[2] == 'select_tag':
-        logging.info('move_tag step 1')
+        logging.info('move_tag step select_tag')
+        # Changing the view_mode in order to change the callback_mode on the displayed tags.
         await state.update_data(view_mode = 'delete_tag')
         await tag_system.invoke_tag_system(callback_query, state, tag_id=tag_id)
+
+    # Confirmation of tag deletion 
     elif parameters[2] == 'confirm':
         markup = InlineKeyboardMarkup(row_width=2)
         markup.add(InlineKeyboardButton("Да", callback_data=f"delete_tag:{parameters[1]}:delete"))
@@ -57,12 +61,13 @@ async def delete_tag(callback_query: types.CallbackQuery, state: FSMContext):
                                     text=f'Вы уверены что хотите удалить тег {parameters[3]}',
                                     reply_markup=markup,
                                     parse_mode=ParseMode.MARKDOWN)
+    # Start deletion
     elif parameters[2] == 'delete':
-        # start delete
         logging.info('|tag_system_functions/delete_tag| tag deleting has started')
         await tags.delete_tag(tag_id=tag_id)
         await state.update_data(view_mode = 'default')
         await tag_system.invoke_tag_system(callback_query, state)
+        logging.info('|tag_system_functions/delete_tag| tag deleting has finished')
 
 
 def tag_system_functions_handlers(dp: Dispatcher):
