71b8ef7c60486f06a900f3df568fbf8dca31da54
Alexander Bykov <113583500+AlexanderGhosty@users.noreply.github.com>
2023-12-03 19:38:57 +0300

    Reduced amount of code due to use a constant table

--- Db/db_messages.py
+++ Db/db_messages.py
@@ -11,36 +11,22 @@ engine = asyncio_ext.create_async_engine(
     future=True
 )
 
-async def createTable():
-    metadata = MetaData()
-    table = Table("messages", metadata,
-                  Column('id', Integer, primary_key=True),
-                  Column('group_id', Integer),
-                  Column('title', VARCHAR(255)),
-                  Column('text', String),
-                  Column('tag_id', Integer),
-                  Column('images', String),
-                  Column('videos', String),
-                  Column('files', String),
-                  Column('created_at', Date))
-    async with engine.begin() as conn:
-        await conn.run_sync(metadata.create_all)
-        await conn.commit()
+metadata = MetaData()
+MESSAGE_TABLE = Table("messages", metadata,
+                Column('id', Integer, primary_key=True),
+                Column('group_id', Integer),
+                Column('title', VARCHAR),
+                Column('text', VARCHAR),
+                Column('tag_id', Integer),
+                Column('images', VARCHAR),
+                Column('videos', VARCHAR),
+                Column('files', VARCHAR),
+                Column('created_at', Date))
 
 async def insert_messages(group_id: int, title: str, text: str, tag_id: int, images: str, videos: str, files: str,
                           created_at):
     async with engine.begin() as conn:
-        metadata = MetaData()
-        table = Table("messages", metadata,
-                      Column('id', Integer, primary_key=True),
-                      Column('group_id', Integer),
-                      Column('title', VARCHAR),
-                      Column('text', VARCHAR),
-                      Column('tag_id', Integer),
-                      Column('images', VARCHAR),
-                      Column('videos', VARCHAR),
-                      Column('files', VARCHAR),
-                      Column('created_at', Date))
+        table = MESSAGE_TABLE
         insertStmt = table.insert().values(group_id=group_id, title=title, text=text, tag_id=tag_id, images=images, \
                                            videos=videos, files=files, created_at=created_at)
         await conn.execute(insertStmt)
@@ -48,18 +34,7 @@ async def insert_messages(group_id: int, title: str, text: str, tag_id: int, ima
 
 async def delete_messages_by_group_id(group_id: int):
     async with engine.begin() as conn:
-        metadata = MetaData()
-        table = Table("messages", metadata,
-                      Column('id', Integer, primary_key=True),
-                      Column('group_id', Integer),
-                      Column('title', VARCHAR),
-                      Column('text', VARCHAR),
-                      Column('tag_id', Integer),
-                      Column('images', VARCHAR),
-                      Column('videos', VARCHAR),
-                      Column('files', VARCHAR),
-                      Column('created_at', Date))
-        
+        table = MESSAGE_TABLE
         deleteStmt = table.delete().where(table.c.group_id == group_id)
 
         await conn.execute(deleteStmt)
@@ -68,17 +43,7 @@ async def delete_messages_by_group_id(group_id: int):
 
 async def fetch_messages_by_tag(tag_id: int):
     async with engine.connect() as conn:
-        metadata = MetaData()
-        table = Table("messages", metadata,
-                      Column('id', BIGINT, primary_key=True),
-                      Column('group_id', BIGINT),
-                      Column('title', VARCHAR),
-                      Column('text', VARCHAR),
-                      Column('tag_id', BIGINT),
-                      Column('images', VARCHAR),
-                      Column('videos', VARCHAR),
-                      Column('files', VARCHAR),
-                      Column('created_at', Date))
+        table = MESSAGE_TABLE
         selectStmt = select(table).where(table.c.tag_id == tag_id)
         result = await conn.execute(selectStmt)
         messages = result.fetchall()
@@ -87,18 +52,7 @@ async def fetch_messages_by_tag(tag_id: int):
 
 async def delete_messages_by_id(id: int):
     async with engine.begin() as conn:
-        metadata = MetaData()
-        table = Table("messages", metadata,
-                      Column('id', BIGINT, primary_key=True),
-                      Column('group_id', BIGINT),
-                      Column('title', VARCHAR),
-                      Column('text', VARCHAR),
-                      Column('tag_id', BIGINT),
-                      Column('images', VARCHAR),
-                      Column('videos', VARCHAR),
-                      Column('files', VARCHAR),
-                      Column('created_at', Date))
-
+        table = MESSAGE_TABLE
         deleteStmt = table.delete().where(table.c.id == id)
 
         await conn.execute(deleteStmt)
